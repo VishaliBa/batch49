0a7bf72c4a38187aeaf1711b40e51cf9aaa2a86a
Vishali.Ba <vishali.bagga21@gmail.com>
2022-10-19 11:29:16 -0400

    Wall & Point

--- src/UdemySection6/Main.java
+++ src/UdemySection6/Main.java
@@ -37,6 +37,32 @@ public class Main {
 
 
 
+                            // WALL  //
+
+//        Wall wall = new Wall(5,4);
+//
+//        System.out.println("height= " + wall.getArea());
+
+         Wall wall = new Wall(5,4);
+         System.out.println("area= " + wall.getArea());
+
+         wall.setHeight(-1.5);
+         System.out.println("width= " + wall.getWidth());
+         System.out.println("height= " + wall.getHeight());
+         System.out.println("area= " + wall.getArea());
+
+                // POINT //
+
+        Point first = new Point(6, 5);
+        Point second = new Point(3, 1);
+        System.out.println("distance(0,0)= " + first.distance());
+        System.out.println("distance(second)= " + first.distance(second));
+        System.out.println("distance(2,2)= " + first.distance(2, 2));
+        Point point = new Point();
+        System.out.println("distance()= " + point.distance());
+
+
+
     }
 
 
--- src/UdemySection6/Person.java
+++ src/UdemySection6/Person.java
@@ -48,4 +48,3 @@ public class Person {
 
 }
 
-//  discuss with Bagga g
